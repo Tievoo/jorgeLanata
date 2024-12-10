49f805202cb6a3b67b4dcd645f2a78abb8892714
Tievoo <tievolib@gmail.com>
2024-12-10 04:00:57 -0300

    info

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,5 +4,16 @@ module.exports = {
     script: "bot.ts",
     interpreter: "deno",
     interpreter_args: "run --allow-env --allow-net --allow-read --allow-write",
-  }]
+  }],
+  deploy : {
+    lanata: {
+      user: 'root',
+      host: ['165.232.148.166'],
+      ref: 'origin/master',
+      repo: 'git@github.com:Tievoo/jorgeLanata.git',
+      path: '/app',
+      'pre-deploy-local': '',
+      'post-deploy': 'yarn && pm2 startOrRestart ecosystem.config.cjs',
+    }
+  }
 };
